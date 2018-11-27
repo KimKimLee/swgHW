35d43380bed656aab648e85a435f2df6b19db296
miguellll <qwqw999@naver.com>
2018-11-27 20:13:49 +0900

    p

---
+++ menu1ui.java
@@ -0,0 +1,27 @@
+package soso;
+
+
+
+import java.util.*;
+import java.io.*;
+
+
+
+
+public class Phonebook{
+   public static void main(String[] args) throws Exception {
+      // TODO Auto-generated method stub
+      HashMap<Integer,Phonebook> h = new HashMap< Integer, Phonebook>(); 
+      Scanner scanner = new Scanner(System.in);
+      //to do list 생성,수정,삭제 => 쓴 날짜,due date,내용
+         int memNO =1;
+         int num =0 ;
+      while(true) {
+         System.out.println("1. to do list 생성");
+         System.out.println("2. to do list 보기 ");
+         System.out.println("3. to do list 삭제");
+         System.out.println("4. to do list 수정");
+         System.out.println("5. 종료");
+         System.out.print("메뉴 번호>>");
+         
+        } }}
