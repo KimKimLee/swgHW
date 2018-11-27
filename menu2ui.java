98d075cf48f5d4cbfcc353f1c8de78aee14b492c
Kimgakim <rkdudrn22@daum.net>
2018-11-27 20:22:11 +0900

    include getter, setter

--- menu2ui.java
+++ menu2ui.java
@@ -2,6 +2,38 @@
 import java.util.*;
 import java.io.*;
 
+
+class Todolist implements Serializable{
+	int num;
+	String w_date;
+	String d_date;
+	String detail;
+
+	public String getw_date() {
+		return w_date;
+	}
+	public void setw_date(String w_date) {
+		this.w_date = w_date;
+	}	
+	public String getd_date() {
+		return d_date;
+	}
+	public void setd_date(String d_date) {
+		this.d_date = d_date;
+	}
+	public String getdetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+	public Todolist(String w_date,String d_date,String detail) {
+		this.w_date=w_date;
+		this.d_date=d_date;
+		this.detail =detail;
+	}
+}
+
 public class menu2{
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
